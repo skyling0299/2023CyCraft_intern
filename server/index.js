e33031a789a5e116264080ef95df6f9018f2885f
skyling0299 <stephaniechu828@gmail.com>
2023-03-14 13:50:35 +0800

    [ADD] the app can run and translate in server side

--- server/index.js
+++ server/index.js
@@ -2,25 +2,49 @@ import express from 'express';
 import * as dotenv from 'dotenv'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from 'openai';
+import fs from 'fs'
+import os from 'os'
 
-dotenv.config();
-const configuration = new Configuration({
+dotenv.config({ override: true });
+let configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
+let openai = new OpenAIApi(configuration);
+
+function setEnvValue(key, value) {
+
+    const ENV_VARS = fs.readFileSync("./.env", "utf8").split(os.EOL);
+    const target = ENV_VARS.indexOf(ENV_VARS.find((line) => {
+        return line.match(new RegExp(key));
+    }));
+    ENV_VARS.splice(target, 1, `${key} = "${value}"`);
+    fs.writeFileSync("./.env", ENV_VARS.join(os.EOL));
+}
 
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 
+app.get('/set', function (req, res) {
+    res.send('Hello World');
+ });
 
-  
+app.post('/set', async(req, res) =>{
+    console.log(req.body.key.content)
+    setEnvValue("OPENAI_API_KEY", req.body.key.content);
+    configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    openai = new OpenAIApi(configuration);
+    res.status(200).send("i got it")
+})
+
 app.post('/', async(req, res) => {
   
     try{
         const prompts = req.body.prompt
-        console.log(prompts.length/2)
+
         const response = await openai.createCompletion({
             model: "text-davinci-002", 
             prompt: prompts,
@@ -29,7 +53,7 @@ app.post('/', async(req, res) => {
         }).then(function(aiResponse){
             if(aiResponse.statusText == 'OK'){
                 res.status(200).send({
-                    translated : aiResponse.data.choices
+                    translated : aiResponse.data.choices[0]
                 })
             }
             else{
@@ -38,7 +62,8 @@ app.post('/', async(req, res) => {
         })
     }
     catch(e){
-        console.log(e.response.data)
+        console.log(e)
+        
         res.status(500).send(e || 'Something went wrong')
     }
 })
